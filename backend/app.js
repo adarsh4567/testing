8fb193e204164f327d262b466a872721f9feafcd
Adarsh <adarsh800251@gmail.com>
2023-01-11 14:46:35 +0530

    solved cors

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const cors = require('cors');
 dotenv.config();
-
+app.use(cors())
 app.use(express.json());
 
 const PORT = 5000 || process.env.PORT;
